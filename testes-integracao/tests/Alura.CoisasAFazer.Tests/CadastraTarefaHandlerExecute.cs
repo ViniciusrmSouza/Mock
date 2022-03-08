2018dd8245516bd783355c07b1913341446fd246
ViniciuSouza <vinicius.napista@napista.com.br>
2022-03-08 14:59:33 -0300

    Utilizando EFInMemory.
    Inicio.

--- testes-integracao/tests/Alura.CoisasAFazer.Tests/CadastraTarefaHandlerExecute.cs
+++ testes-integracao/tests/Alura.CoisasAFazer.Tests/CadastraTarefaHandlerExecute.cs
@@ -2,7 +2,9 @@ using System;
 using System.Linq;
 using Alura.CoisasAFazer.Core.Commands;
 using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Infrastructure;
 using Alura.CoisasAFazer.Services.Handlers;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Alura.CoisasAFazer.Tests
@@ -14,7 +16,10 @@ namespace Alura.CoisasAFazer.Tests
         {
             //Arrange
             var command = new CadastraTarefa("Estudar Mock", new Categoria("Estudo"), new DateTime(2022, 03, 15));
-            var repo = new MockRepository();
+
+            var options = new DbContextOptionsBuilder<DbTarefasContext>().UseInMemoryDatabase("DbTeste").Options;
+            var context = new DbTarefasContext(options);
+            var repo = new RepositorioTarefa(context);
             var handler = new CadastraTarefaHandler(repo); 
 
             //Act
