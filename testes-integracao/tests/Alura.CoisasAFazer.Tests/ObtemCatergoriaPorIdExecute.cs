7f2b1b81444f773cb014fb4e051c9170e85a9586
ViniciusSouza <vinicius.souza@napista.com.br>
2022-03-24 23:50:35 -0300

    Testando o obtem categoria por id.

---
+++ testes-integracao/tests/Alura.CoisasAFazer.Tests/ObtemCatergoriaPorIdExecute.cs
@@ -0,0 +1,35 @@
+﻿using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Infrastructure;
+using Alura.CoisasAFazer.Services.Handlers;
+using Moq;
+using Xunit;
+
+namespace Alura.CoisasAFazer.Tests
+{
+    public class ObtemCatergoriaPorIdExecute
+    {
+        [Fact]
+        public void ObtemCategoria_IdExistente_Sucesso()
+        {
+            //Arrange
+            var categoriaId = 1;
+            var comando = new ObtemCategoriaPorId(categoriaId);
+
+            var mock = new Mock<IRepositorioTarefas>();
+            mock.Setup(r => r.ObtemCategoriaPorId(It.Is<int>(x=> x == 1)))
+                .Returns(new Categoria(1,"teste"));
+            
+            var repo = mock.Object;
+            var handler = new ObtemCategoriaPorIdHandler(repo);
+            
+            //Act
+            var result = handler.Execute(comando);
+            
+            //Assert
+            mock.Verify(r => r.ObtemCategoriaPorId(categoriaId), 
+                Times.Once);
+            Assert.Equal(categoriaId, result.Id);
+        }
+    }
+}
